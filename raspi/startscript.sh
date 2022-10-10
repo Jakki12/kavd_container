e519d1c97a14859192eda6c6c415e2a0d8f2321f
Jakki12 <jakkesprinter@web.de>
2022-10-10 09:53:22 +0200

    deleted

--- raspi/startscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#/usr/bin/python /home/pi/theLAB/raspi/led_blink.py
-#/usr/bin/pd -nogui -open /home/pi/Desktop/shell_test.pd
-#nimm das hier unten
-#/usr/bin/pd -nogui -rt -open /home/pi/theLAB/pd/LAB_main.pd
