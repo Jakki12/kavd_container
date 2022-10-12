5165928cfadf5492088a78bafeaec0f8b41f2e47
Jakki12 <jakkesprinter@web.de>
2022-10-12 13:42:42 +0200

    neues vom raspi

--- raspi/startscript.sh
+++ raspi/startscript.sh
@@ -3,4 +3,12 @@
 #/usr/bin/python /home/pi/theLAB/raspi/led_blink.py
 #/usr/bin/pd -nogui -open /home/pi/Desktop/shell_test.pd
 #nimm das hier unten
-#/usr/bin/pd -nogui -rt -open /home/pi/theLAB/pd/LAB_main.pd
+#/usr/bin/pd -nogui -rt -open /home/pi/kavd_container/pd/LAB_main.pd
+#/usr/bin/pd -nogui -rt -open /home/pi/kavd_container/pd/synthtest.pd
+
+
+pulseaudio --kill
+
+sleep 3
+
+sudo /usr/bin/pd -nogui -rt /home/pi/kavd_container/pd/synthtest.pd
