7fbe8ca8c70944763039759be42e00a4180851a5
Jakki12 <jakkesprinter@web.de>
2022-10-13 09:42:00 +0200

    on raspi: added raspi/mac distinction

--- raspi/startscript.sh
+++ raspi/startscript.sh
@@ -5,10 +5,4 @@
 #nimm das hier unten
 #/usr/bin/pd -nogui -rt -open /home/pi/kavd_container/pd/LAB_main.pd
 #/usr/bin/pd -nogui -rt -open /home/pi/kavd_container/pd/synthtest.pd
-
-
-pulseaudio --kill
-
-sleep 3
-
-sudo /usr/bin/pd -nogui -rt /home/pi/kavd_container/pd/synthtest.pd
+sudo /usr/bin/pd -nogui -rt -open /home/pi/kavd_container/pd/LAB_main.pd -send "raspiOrNot 1"
